22dd693ae30b4917039a0234132c17e1b5a795d9
Maksim Dahovnik <dahovnik@gmail.com>
2024-07-23 19:40:02 +0200

    Add, remove cards function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardItem, deleteCard) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+
+    card.querySelector('.card__image').src = cardItem.link;
+    card.querySelector('.card__title').textContent = cardItem.name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => {
+        deleteCard(card);
+    });
+
+    return card;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardItem => {
+    placesList.append(createCard(cardItem, deleteCard));
+});
+
