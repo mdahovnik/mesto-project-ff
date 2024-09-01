2a94601653ec28b820be4db94ba8215431182d7e
Maksim Dahovnik <dahovnik@gmail.com>
2024-09-01 18:44:13 +0200

    Fix no-scroll on popup open

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,20 +1,17 @@
 export { openPopup, closePopup };
 
 function openPopup(popupType) {
-    document.querySelector('.page').classList.add('page-no-scroll');
-
     popupType.addEventListener('click', handleOnClick);
     document.addEventListener('keydown', handleOnKeydown);
     popupType.classList.add('popup_is-opened');
+    document.querySelector('.page').classList.add('page-no-scroll');
 }
 
 function closePopup(popupType) {
+    popupType.removeEventListener('click', handleOnClick);
+    document.removeEventListener('keydown', handleOnKeydown);
+    popupType.classList.remove('popup_is-opened');
     document.querySelector('.page').classList.remove('page-no-scroll');
-    if (popupType && popupType.classList.contains('popup_is-opened')) {
-        popupType.removeEventListener('click', handleOnClick);
-        document.removeEventListener('keydown', handleOnKeydown);
-        popupType.classList.remove('popup_is-opened');
-    }
 }
 
 function handleOnClick(event) {
